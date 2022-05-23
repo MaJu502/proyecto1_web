14e42f73ccc708330c928924451e153f9cb46a75
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2022-05-23 17:32:57 -0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  ignorePatterns: [
+    'node_modules/',
+    'src/imagepool/',
+    'src/styles/',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': 0,
+    semi: ['error', 'never'],
+    'linebreak-style': 0,
+    'react/function-component-definition': ['error', {
+      namedComponents: 'arrow-function',
+    }],
+    indent: ['off', 'tab'],
+    'import/no-extraneous-dependencies': [0, {
+      devDependencies: ['*/.prop-types.*'],
+      peerDependencies: true,
+    }],
+    'prefer-template': 0,
+    'arrow-parens': [0, 'as-needed'],
+    'react/no-array-index-key': ['off', 'index'],
+    'no-unused-vars': ['off'],
+    'react/jsx-max-props-per-line': ['off'],
+    'react/jsx-first-prop-new-line': ['off'],
+    'import/extensions': ['off'],
+  },
+}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { dirname } = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            cacheDirectory: true,
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+}
