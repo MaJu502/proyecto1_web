b71bd60f5ea66588abb9a2d900ced2db82812ec3
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2022-05-24 14:31:54 -0600

    initial grid

---
+++ src/componentes/Griddisplay.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react'
+import '../styles/Griddisplay.css'
+
+const Grid = () => {
+  const nombre = 'hola'
+
+  return (
+    <div className="grid_elements">
+      <button type="button">hola</button>
+      <button type="button">hola</button>
+      <button type="button">hola</button>
+      <button type="button">hola</button>
+      <button type="button">hola</button>
+      <button type="button">hola</button>
+    </div>
+  )
+}
+
+export default Grid
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Divunicef from './componentes/Unicef.jsx'
 import DivTV from './componentes/Divtv.jsx'
 import Iphone13pro from './componentes/Iphone13pro.jsx'
 import Iphonese from './componentes/Iphonese.jsx'
+import Griddisplay from './componentes/Griddisplay.jsx'
 import './styles/Master.css'
 
 const App = () => (
@@ -17,6 +18,8 @@ const App = () => (
     <Iphone13pro />
     <br />
     <Iphonese />
+    <br />
+    <Griddisplay />
   </nav>
 )
 
